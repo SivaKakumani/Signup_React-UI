1758156cbd949b580bc7b193c4b45008487258f2
Venkata Kakumani <venkata@govtechpros.com>
2024-05-15 14:51:34 -0400

    pusing all the files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Signup from './Components/Signup'
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='flex w-full h-screen'>
+      <div className="w-full flex items-center justify-center ">
+      <Signup/>
+      </div>
+      {/* <div className='hidden lg:flex relative w-1/2 h-full items-center justify-center bg-gray-200'>
+        <div className='w-60 h-60 bg-gradient-to-tr from-violet-500 to-pink-500 rounded-full animate-spin'/>
+        <div className='w-full h-1/2 absolute bottom-0 bg-white/10 backdrop-blur-lg'/>
+      </div> */}
+      
     </div>
-  );
+    
+  )
 }
-
-export default App;
